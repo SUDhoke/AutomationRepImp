77d42a9aad1736eb037fa6515ecd7a2387e76624
HP <dhokesona25@gmail.com>
2025-01-05 13:59:18 +0530

    program added for println vs print

---
+++ src/Lab_002_println_vs_print.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class Lab_002_println_vs_print
+{
+    public static void main(String[] args)
+    {
+       // program is for checking println and print
+        System.out.println("Hello, println is used for printing in new line");
+        System.out.println("details here are printed in second line");
+        System.out.println("this details get printed in third line in console\n\n");
+
+
+        // checking the print
+        System.out.print("all details enter here is printed in a single line ");
+        System.out.print("  ,will print data in same line");
+    }
+}
