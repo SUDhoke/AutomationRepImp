9bc80cef726be8d95ab9a1bcf4594edb57c68f11
HP <dhokesona25@gmail.com>
2025-01-05 14:17:30 +0530

    addition of two integer and a float number

--- src/Lab_003_addition_Input.java
+++ src/Lab_003_addition_Input.java
@@ -3,15 +3,18 @@ public class Lab_003_addition_Input {
     public static void main(String[] args) {
 // declaration of variables
         //int is a datatye
-        // a,b,c are variables
-        int a,b,c;
+        // a,b,c,d are variables
+        int a,b;
+        float c,d;
 
         Scanner sonali=new Scanner(System.in);
         System.out.println("Enter first value");
         a=sonali.nextInt();
         System.out.println("Enter Second value");
         b=sonali.nextInt();
-        c=a+b;
+        System.out.println("Enter third value");
+        d=sonali.nextFloat();
+        c=a+b+d;
         System.out.println("Addition of two integer numbers\n"+c);
     }
 }
