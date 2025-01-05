088f773540482027c90d0b459441752f9d375a65
HP <dhokesona25@gmail.com>
2025-01-05 14:13:12 +0530

    addition of two number

---
+++ src/Lab_003_addition_Input.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Lab_003_addition_Input {
+    public static void main(String[] args) {
+// declaration of variables
+        //int is a datatye
+        // a,b,c are variables
+        int a,b,c;
+
+        Scanner sonali=new Scanner(System.in);
+        System.out.println("Enter first value");
+        a=sonali.nextInt();
+        System.out.println("Enter Second value");
+        b=sonali.nextInt();
+        c=a+b;
+        System.out.println("Addition of two integer numbers\n96358"+c);
+    }
+}
